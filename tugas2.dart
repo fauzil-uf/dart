afb9170260ef4db141964eede48d64c5995ccb5f
fauzil-uf <fauzil-uf>
2026-07-20 07:59:34 +0700

    final

--- tugas2.dart
+++ tugas2.dart
@@ -4,12 +4,15 @@ bool cekKelulusan(double rataRata, int uts, int uas, double kehadiran) {
 }
 
 void main() {
-  int uts = 60;
-  int uas = 80;
-  double kehadiran = 85.0;
+  int uts = 70;
+  int uas = 75;
+  double kehadiran = 82.5;
 
   double rataRata = (uts + uas) / 2;
-
+  print("Nilai UTS: $uts");
+  print("Nilai UAS: $uas");
+  print("Nilai rata-rata (UTS dan UAS) : $rataRata");
+  print("kehadiran: $kehadiran %");
   if (cekKelulusan(rataRata, uts, uas, kehadiran)) {
     print("Mahasiswa LULUS");
   } else {
